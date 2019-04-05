8a89c41f5200e7cd407c2f223425cf753cca9ee2
Vitor Emanuel <veslima3@gmail.com>
2019-04-05 11:54:21 -0300

    Use class Syntax to Define a Constructor Function
    ES6 provides a new syntax to help create objects, using the keyword class.
    
    This is to be noted, that the class syntax is just a syntax, and not a full-fledged class based implementation of object oriented paradigm, unlike in languages like Java, or Python, or Ruby etc.
    
    In ES5, we usually define a constructor function, and use the new keyword to instantiate an object.
    
    var SpaceShuttle = function(targetPlanet){
      this.targetPlanet = targetPlanet;
    }
    var zeus = new SpaceShuttle('Jupiter');
    The class syntax simply replaces the constructor function creation:
    
    class SpaceShuttle {
      constructor(targetPlanet){
        this.targetPlanet = targetPlanet;
      }
    }
    const zeus = new SpaceShuttle('Jupiter');
    Notice that the class keyword declares a new function, and a constructor was added, which would be invoked when new is called - to create a new object.
    
    
    Use class keyword and write a proper constructor to create the Vegetable class.
    
    The Vegetable lets you create a vegetable object, with a property name, to be passed to constructor.

---
+++ Use class Syntax to Define a Constructor Function.js
@@ -0,0 +1,14 @@
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+  constructor(Vegetable){
+  this.name = Vegetable;
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
